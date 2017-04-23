6dffcda0d5e2e260d47ddefb4a824fe641a2c5ba
= <nncl@live.com>
2017-04-23 11:32:02 -0300

    Restructuring project, assets and stuff

--- ConceitosBootstrap/js/app.js
+++ ConceitosBootstrap/js/app.js
@@ -1,4 +1,12 @@
-﻿var cursos = [{
+﻿/**
+ * @description
+ * Funcionalidades:
+ *  Listagem de cursos disponíveis
+ *  Registro de curso por estudante
+ *  Listagem de cursos registrados localmente via IndexedDB
+ */
+
+var cursos = [{
     "id": "curso-1",
     "titulo": "Desenvolvimento de Soluções"
 }, {
@@ -20,13 +28,23 @@
 
 var lista = document.getElementById("curso");
 
-function limparLista() {
+/**
+ * @description
+ * Limpa a lista de cursos do select.
+ */
+
+var limparLista = function () {
     while (lista.firstChild) {
         lista.removeChild(lista.firstChild);
     }
-}
+};
 
-function exibirCursos() {
+/**
+ * @description
+ * Exibe os cursos no select
+ */
+
+var exibirCursos = function () {
     limparLista();
 
     for (var i = 0; i < cursos.length; i++) {
@@ -35,82 +53,33 @@ function exibirCursos() {
         option.setAttribute("value", cursos[i].id);
         lista.appendChild(option);
     }
-}
+}();
 
-// Setup DB
-window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+/**
+ * @description
+ * Salva registro de curso de um usuário
+ * localmente.
+ */
 
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange
+var saveCourse = function () {
 
-if (!window.indexedDB) {
-    window.alert("Your browser doesn't support a stable version of IndexedDB.")
-}
+    $('#courseForm').submit(function (e) {
+        e.preventDefault();
 
-// window.indexedDB.deleteDatabase("fiap_cursos");
-var request = window.indexedDB.open("fiap_cursos", 1);
-var db;
+        var select = document.getElementById("curso"),
+            name = document.getElementById("f_name").value,
+            email = document.getElementById("f_email").value,
+            courseId = select.options[select.selectedIndex].value,
+            courseTitle = select.options[select.selectedIndex].text,
+            obj = {
+                "id": courseId,
+                "title": courseTitle,
+                "name": name,
+                "email": email,
+                "created_at": new Date()
+            };
 
-request.onsuccess = function (event) {
-    db = request.result;
-};
+        console.log(obj);
 
-request.onupgradeneeded = function (event) {
-    var db = event.target.result;
-    var objectStore = db.createObjectStore("course", { keyPath: "id" });
-
-    /*for (var i in employeeData) {
-        objectStore.add(employeeData[i]);
-    }*/
-
-    readAll();
-}
-
-function saveCourse() {
-    // Get course from id=course select
-    var select = document.getElementById("curso"),
-        name = document.getElementById("f_name").value,
-        email = document.getElementById("f_email").value,
-        courseId = select.options[select.selectedIndex].value,
-        courseTitle = select.options[select.selectedIndex].text,
-        obj = {
-            "id": courseId,
-            "title": courseTitle,
-            "name": name,
-            "email": email,
-            "created_at": new Date()
-        };
-
-    // Save into DB
-    var request = db.transaction(["course"], "readwrite").objectStore("course").add(obj);
-    
-    request.onsuccess = function (event) {
-        alert("Curso salvo com sucesso");
-    };
-
-    request.onerror = function (event) {
-        alert("Erro ao salvar curso");
-    }
-
-    return false;
-}
-
-function readAll() {
-    console.log("readAll");
-    var objectStore = db.transaction("course").objectStore("course");
-
-    objectStore.openCursor().onsuccess = function (event) {
-        var cursor = event.target.result;
-
-        if (cursor) {
-            console.log("Name for id " + cursor.key + " is " + cursor.value.name + ", Age: " + cursor.value.title + ", Email: " + cursor.value.email);
-            cursor.continue();
-        }
-
-        else {
-            console.log("No more entries!");
-        }
-    };
-}
-
-exibirCursos();
+    });
+}();
